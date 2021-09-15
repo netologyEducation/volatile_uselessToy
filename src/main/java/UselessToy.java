75e0306a8b5fbbaa47ee6133491547c912a7a724
noname <fromchita@yandex.ru>
2021-09-15 11:57:41 +0300

    отформатировал код

--- src/main/java/UselessToy.java
+++ src/main/java/UselessToy.java
@@ -14,25 +14,25 @@ public class UselessToy {
         this.sleepTimeOut = sleepTimeOut;
     }
 
-    protected void switchOn(){
-        while (Thread.currentThread().isAlive() && gameIteration >= 0){
-        try {
-            Thread.sleep(sleepTimeOut);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    protected void switchOn() {
+        while (Thread.currentThread().isAlive() && gameIteration >= 0) {
+            try {
+                Thread.sleep(sleepTimeOut);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (!switchKey) {
+                System.out.printf("---Включил %s\n", Thread.currentThread().getName());
+                switchKey = true;
+                gameIteration--;
+            }
         }
-        if(!switchKey){
-            System.out.printf("---Включил %s\n", Thread.currentThread().getName());
-            switchKey = true;
-            gameIteration--;
-        }
-    }
         Thread.currentThread().interrupt();
-}
+    }
 
-    protected void switchOff(){
-        while (Thread.currentThread().isAlive() && gameIteration >= 0){
-            if (switchKey){
+    protected void switchOff() {
+        while (Thread.currentThread().isAlive() && gameIteration >= 0) {
+            if (switchKey) {
                 System.out.printf("------Выключил %s\n", Thread.currentThread().getName());
                 switchKey = false;
             }
